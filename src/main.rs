602c6853e71c0c5797d438ef3f2627d91b2235dc
Piotr Kopalko <piotr.kopalko@yougov.com>
2022-11-10 20:20:33 +0100

    Delivery always takes 5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn deliver(cargo: Vec<char>) -> i8 {
+    return 5
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::deliver;
+    #[test]
+    fn counts_time_needed_to_deliver_one_cargo_to_factory_b() {
+        let cargo = vec!['B'];
+        let time = deliver(cargo);
+        assert_eq!(time, 5);
+    }
+}
